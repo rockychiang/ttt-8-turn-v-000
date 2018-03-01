d0a0e57c538e67d31ec17cd03c5dd96aa95c0e34
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-01 23:45:21 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,3 +9,13 @@ def display_board(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
   end
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+def valid_move?(board, index)
+  if index < 0 | index > 8
+    false
+  elsif board[index] == " " | board[index] == "" | board[index] == nil
+    true
+  else
+    false
+  end
+end
