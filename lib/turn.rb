fabc3d997b85a6de439f401c82de9b3d9ae1d814
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-01 23:58:47 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,10 +1,10 @@
 def display_board(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  11.Times do
+  11.times do
     print "-"
   end
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  11.Times do
+  11.times do
     print "-"
   end
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
